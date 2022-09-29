d44ce2abf5072707938d9f99130ba39204c2ec88
Nazma12345y <114631139+Nazma12345y@users.noreply.github.com>
2022-09-29 11:33:09 +0700

    Add files via upload

---
+++ AplikasiBiodata.py
@@ -0,0 +1,26 @@
+# aplikasi  biodata creat by nazma
+
+#Input nama
+nama = input ("masukkan nama anda !")
+#output nama
+print ("nama anda adalah : ",nama)
+
+#input kelas
+kelas = input ("masukkan kelas?")
+#output kelas 
+print("kelas anda adalah :", kelas)
+
+#input nim
+nim = input("masukkan nim!")
+#output nim
+print("nim anda adalah : ",nim)
+
+#input prodi
+prodi = input ("masukkan prodi!")
+#output prodi
+print("anda dari prodi;",prodi)
+
+#input tanggal lahir
+tanggallahir = input ("masukkan prodi!")
+#output prodi
+print("anda dari prodi;",tanggallahir)
