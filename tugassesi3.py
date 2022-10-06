a4c2ed4a4725b0d1284df7b3f29d1ad6838437b1
Nazma12345y <114631139+Nazma12345y@users.noreply.github.com>
2022-10-06 09:17:56 +0700

    Add files via upload

---
+++ tugassesi3.py
@@ -0,0 +1,7 @@
+# tugas sesi 3 by Nazma
+nama = "Nazma"
+kelas = "TI22A"
+prodi = "Teknk Informatika"
+quote = "pertama, berpikirlah. yang kedua, percayalah. yang ketiga, mimpikanlah. lalu, yang terakhir, tantanglah."
+
+print(nama, kelas ,prodi, quote)
